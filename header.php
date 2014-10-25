53b359ea7bc796a5e7c353060d10ddf7707b392e
Takanori Watanabe (PC) <takanori@umich.edu>
2014-10-25 13:19:03 -0400

    Can't make up my mind with the nav-bar style...created separate php files for this (selection controlled via header.php)

--- header.php
+++ header.php
@@ -23,54 +23,11 @@
     
     <body>
     <div class="container">
-        <!-- new nav bar (10/25/2014) -->
-        <div class="container header">
-            <div>
-                <h3 class="text-muted my-proj-name">Takanori Watanabe</h3>
-                    <ul class="nav navbar-collapse collapse my-social">
-                        <li><a href="mailto:takanori@umich.edu" target="_top" ><img title="takanori@umich.edu" src="./img/social-media-icon/email.png" height="30" /></a></li>
-                        <!--<li><a href="https://www.facebook.com/takanori.watanabe.23" target="_blank" ><img title="Facebook" src="img/social-media-icon/facebook.png" height="30" /></a></li>-->                        
-                        <li><a href="https://github.com/takwatanabe2004" target="_blank" ><img title="Github" src="img/social-media-icon/GitHub-Mark-120px-plus.png" height="30" /></a></li>
-                        <li><a href="http://www.linkedin.com/pub/takanori-watanabe/9b/588/355" target="_blank" ><img title="Linkedin" src="img/social-media-icon/linkedin.png" height="30" /></a></li>
-                        <li><a href="http://scholar.google.com/citations?hl=en&user=tGMrt6sAAAAJ" target="_blank" ><img title="Google Scholar" src="img/social-media-icon/google-scholar.jpg" height="30" /></a></li>                        
-                    </ul>                    
-            </div>
-
-          <div class="masthead">
-            <ul class="nav nav-justified">
-                <li><a href="index-new-php.php">Home</a></li>
-                <li><a href="about.php">About</a></li>
-                <li><a href="research.php">Research</a></li>
-                <li><a href="courses.php">Courses</a></li>
-                <li><a href="contact.php">Contact Info</a></li>
-            </ul>
-          </div>
-        </div>
-        <!-- old navbar div (10/25/2014) -->
-<!--        <div class="navbar navbar-inverse navbar-top" role="navigation">
-            <div class="container">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand">Takanori Watanabe</a>
-                </div>
-                <div class="container">
-                    <ul class="nav navbar-nav">
-                        <li><a href="index-new-php.php">Home</a></li>
-                        <li><a href="about.php">About</a></li>
-                        <li><a href="research.php">Research</a></li>
-                        <li><a href="courses.php">Courses</a></li>
-                        <li><a href="contact.php">Contact Info</a></li>
-                    </ul>
-                </div>/.nav-collapse 
-            </div>
-        </div>-->
-        <!-- end navbar div -->
         
+        <?php include('navbar-white.php'); ?>
+        <?php // include('navbar-black.php'); ?>
+        
+
 
 
         <!-- Begin main container -->
